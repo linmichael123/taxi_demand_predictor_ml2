da082434e75e23351695f97434eb68d50659c357
Mike <michael.tz.lin@gmail.com>
2024-02-17 21:47:11 -0800

    deploy

--- src/config.py
+++ src/config.py
@@ -16,7 +16,7 @@ FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
 FEATURE_GROUP_VERSION = 2
 FEATURE_GROUP_METADATA = FeatureGroupConfig(
     name='time_series_hourly_feature_group',
-    version=3,
+    version=2,
     description='Feature group with hourly time-series data of historical taxi rides',
     primary_key=['pickup_location_id', 'pickup_ts'],
     event_time='pickup_ts',
@@ -27,13 +27,14 @@ FEATURE_VIEW_NAME = 'time_series_hourly_feature_view'
 FEATURE_VIEW_VERSION = 2
 FEATURE_VIEW_METADATA = FeatureViewConfig(
     name='time_series_hourly_feature_view',
-    version=3,
+    version=2,
     feature_group=FEATURE_GROUP_METADATA,
 )
 
 
 MODEL_NAME = "taxi_demand_predictor_next_hour"
-MODEL_VERSION = 1
+MODEL_VERSION = 3
+
 FEATURE_GROUP_MODEL_PREDICTIONS = 'model_predictions_feature_group'
 FEATURE_GROUP_PREDICTIONS_METADATA = FeatureGroupConfig(
     name='model_predictions_feature_group',
